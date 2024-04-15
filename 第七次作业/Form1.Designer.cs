8ce0cabc21c537e437c593aca6ff666a7b52ad00
99044 <127596376+99044@users.noreply.github.com>
2024-04-15 21:36:05 +0800

    Add files via upload

---
+++ 第七次作业/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" });
+            this.comboBox1.Location = new System.Drawing.Point(51, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(171, 26);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "depth";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] { "30", "50", "60", "75", "100", "120", "150" });
+            this.comboBox2.Location = new System.Drawing.Point(51, 81);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(171, 26);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.Text = "leng";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] { "0.3", "0.4", "0.5", "0.6", "0.7" });
+            this.comboBox3.Location = new System.Drawing.Point(51, 139);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(171, 26);
+            this.comboBox3.TabIndex = 2;
+            this.comboBox3.Text = "per1";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] { "0.3", "0.4", "0.5", "0.6", "0.7" });
+            this.comboBox4.Location = new System.Drawing.Point(51, 200);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(171, 26);
+            this.comboBox4.TabIndex = 3;
+            this.comboBox4.Text = "per2";
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] { "10", "20", "30", "45" });
+            this.comboBox5.Location = new System.Drawing.Point(51, 269);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(171, 26);
+            this.comboBox5.TabIndex = 4;
+            this.comboBox5.Text = "th1";
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Items.AddRange(new object[] { "10", "20", "30", "45" });
+            this.comboBox6.Location = new System.Drawing.Point(51, 332);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(171, 26);
+            this.comboBox6.TabIndex = 5;
+            this.comboBox6.Text = "th2";
+            this.comboBox6.SelectedIndexChanged += new System.EventHandler(this.comboBox6_SelectedIndexChanged);
+            // 
+            // comboBox7
+            // 
+            this.comboBox7.FormattingEnabled = true;
+            this.comboBox7.Items.AddRange(new object[] { "Blue", "Black", "Yellow", "Green", "Red" });
+            this.comboBox7.Location = new System.Drawing.Point(51, 393);
+            this.comboBox7.Name = "comboBox7";
+            this.comboBox7.Size = new System.Drawing.Size(171, 26);
+            this.comboBox7.TabIndex = 6;
+            this.comboBox7.Text = "pen";
+            this.comboBox7.SelectedIndexChanged += new System.EventHandler(this.comboBox7_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(997, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 60);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "paint";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1626, 1067);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox7);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.ComboBox comboBox7;
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.ComboBox comboBox1;
+
+        #endregion
+    }
+}
---
+++ 第七次作业/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private Graphics graphics;
+        private double th1 = 30 * Math.PI / 180;
+        private double th2 = 20 * Math.PI / 180;
+        private double per1 = 0.6;
+        private double per2 = 0.7;
+        private int depth=10;
+        private double leng=100;
+        private Pen pen;
+
+
+        void drawCayleyTree(int n,double x0,double y0,double leng,double th)
+        {
+            if (n > 0)
+            {
+                double x1 = x0 + leng * Math.Cos(th);
+                double y1 = y0 + leng * Math.Sin(th);
+
+                drawLine(x0, y0, x1, y1);
+                
+                drawCayleyTree(n - 1,x1,y1,per1 * leng,th + th1);
+                drawCayleyTree(n - 1,x1,y1,per2 * leng,th - th2);
+
+            }
+        }
+
+        void drawLine(double x0,double y0,double x1,double y1)
+        {
+            if (pen == null)
+            {
+                pen = new Pen(Color.Blue);
+            }
+            else
+            {
+                graphics.DrawLine(pen,(int) x0,(int) y0,(int) x1,(int) y1);
+            }
+        }
+
+        
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.TabIndex = 10;
+            Controls.Add(comboBox1);
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            if (comboBox1.SelectedItem != null)
+            {
+                depth = int.Parse(comboBox1.SelectedItem.ToString());
+                // 使用 depth 变量来绘制 Cayley 树或进行其他操作
+            }
+            else
+            {
+                depth = 100;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.TabIndex = 5;
+            Controls.Add(comboBox1);
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            if (comboBox2.SelectedItem != null)
+            {
+                leng = int.Parse(comboBox2.SelectedItem.ToString());
+                // 使用 leng 变量来绘制 Cayley 树或进行其他操作
+            }
+            else
+            {
+                leng=100;            
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox3.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox3.FormattingEnabled = true;
+            comboBox3.TabIndex = 3;
+            Controls.Add(comboBox3);
+            comboBox3.SelectedIndexChanged += comboBox3_SelectedIndexChanged;
+            if (comboBox3.SelectedItem != null)
+            {
+                per1 = double.Parse(comboBox3.SelectedItem.ToString());
+                // 使用 per1 变量来绘制 Cayley 树或进行其他操作
+            }
+            else
+            {
+                per1 = 0.6;
+            }
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox4.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox4.FormattingEnabled = true;
+            comboBox4.TabIndex = 4;
+            Controls.Add(comboBox4);
+            comboBox4.SelectedIndexChanged += comboBox4_SelectedIndexChanged;
+            if (comboBox4.SelectedItem != null)
+            {
+                per2 = double.Parse(comboBox4.SelectedItem.ToString());
+                // 使用 per2 变量来绘制 Cayley 树或进行其他操作
+            }
+            else
+            {
+                per2 = 0.7;
+            }
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox5.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox5.FormattingEnabled = true;
+            comboBox5.TabIndex = 1;
+            Controls.Add(comboBox5);
+            comboBox5.SelectedIndexChanged += comboBox5_SelectedIndexChanged;
+            if (comboBox5.SelectedItem != null)
+            {
+                th1 = int.Parse(comboBox5.SelectedItem.ToString())* Math.PI / 180;
+                // 使用 th1 变量来绘制 Cayley 树或进行其他操作
+            }
+            else
+            {
+                th1 = 20 * Math.PI / 180;
+            }
+        }
+
+        private void comboBox6_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox6.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox6.FormattingEnabled = true;
+            comboBox6.TabIndex = 2;
+            Controls.Add(comboBox1);
+            comboBox6.SelectedIndexChanged += comboBox6_SelectedIndexChanged;
+            if (comboBox6.SelectedItem != null)
+            {
+                th2 = int.Parse(comboBox6.SelectedItem.ToString())* Math.PI / 180;
+                // 使用 th2 变量来绘制 Cayley 树或进行其他操作
+            }
+            else
+            {
+                th2 = 30 * Math.PI / 180;
+            }
+        }
+
+        private void comboBox7_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox7.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox7.FormattingEnabled = true;
+            comboBox7.TabIndex = 2;
+            Controls.Add(comboBox1);
+            comboBox7.SelectedIndexChanged += comboBox7_SelectedIndexChanged;
+            if (comboBox7.SelectedItem != null)
+            {
+                switch (comboBox7.SelectedItem.ToString())
+                {
+                    case "Blue" :
+                        pen = new Pen(Brushes.Blue);
+                        break;
+                    case "Black":
+                        pen = new Pen(Color.Black);
+                        break;
+                    case "Red":
+                        pen = new Pen(Color.Red);
+                        break;
+                    case "Yellow":
+                        pen = new Pen(Color.Yellow);
+                        break;
+                    case "Green":
+                        pen = new Pen(Color.Green);
+                        break;
+                    default:
+                        pen = new Pen(Color.Blue);
+                        break;
+                }
+
+                // 使用 pen 变量来绘制 Cayley 树或进行其他操作
+            }
+            else
+            {
+                pen = new Pen(Color.Blue);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (graphics == null)
+            {
+                graphics = this.CreateGraphics();
+            }
+            graphics.Clear(BackColor);
+            drawCayleyTree(depth,500,500,leng,-Math.PI/2);
+        }
+    }
+}
