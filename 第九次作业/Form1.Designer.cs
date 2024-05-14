116cbdefabd56173f7062fd9322637b518689329
99044 <127596376+99044@users.noreply.github.com>
2024-05-14 22:23:50 +0800

    Add files via upload

---
+++ 第九次作业/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnScrape = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(63, 43);
+            this.txtURL.MaximumSize = new System.Drawing.Size(500, 500);
+            this.txtURL.Multiline = true;
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(500, 55);
+            this.txtURL.TabIndex = 0;
+            this.txtURL.Text = "请输入URL";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(63, 197);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(1065, 269);
+            this.txtResult.TabIndex = 1;
+            // 
+            // btnScrape
+            // 
+            this.btnScrape.Location = new System.Drawing.Point(472, 608);
+            this.btnScrape.Name = "btnScrape";
+            this.btnScrape.Size = new System.Drawing.Size(196, 74);
+            this.btnScrape.TabIndex = 2;
+            this.btnScrape.Text = "点击进行查找";
+            this.btnScrape.UseVisualStyleBackColor = true;
+            this.btnScrape.Click += new System.EventHandler(this.btnScrape_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1278, 821);
+            this.Controls.Add(this.btnScrape);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtURL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnScrape;
+
+        #endregion
+    }
+}
---
+++ 第九次作业/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+            private void btnScrape_Click(object sender, EventArgs e)
+            {
+                string url = txtURL.Text;
+
+                // 使用 WebClient 获取页面内容
+                using (WebClient client = new WebClient())
+                {
+                    try
+                    {
+                        string html = client.DownloadString(url);
+
+                        // 使用正则表达式查找手机号码
+                        Regex phoneRegex = new Regex(@"1[3456789]\d{9}");
+                        MatchCollection phoneMatches = phoneRegex.Matches(html);
+
+                        // 使用正则表达式查找邮箱地址
+                        Regex emailRegex = new Regex(@"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}");
+                        MatchCollection emailMatches = emailRegex.Matches(html);
+
+                        // 清空文本框中的内容
+                        txtResult.Text = "";
+
+                        // 显示找到的手机号码
+                        foreach (Match match in phoneMatches)
+                        {
+                            txtResult.AppendText("Phone: " + match.Value + Environment.NewLine);
+                        }
+
+                        // 显示找到的邮箱地址
+                        foreach (Match match in emailMatches)
+                        {
+                            txtResult.AppendText("Email: " + match.Value + Environment.NewLine);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                }
+            }
+        }
+        
+        
+    }
