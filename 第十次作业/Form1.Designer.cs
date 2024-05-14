c58d41ac53611820af52ff893522ee9f04166cc7
99044 <127596376+99044@users.noreply.github.com>
2024-05-14 22:24:45 +0800

    Add files via upload

---
+++ 第十次作业/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxBingResults = new System.Windows.Forms.TextBox();
+            this.textBoxQuery = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxSogouResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(744, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(295, 140);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "点击查找";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // textBoxBingResults
+            // 
+            this.textBoxBingResults.Location = new System.Drawing.Point(98, 522);
+            this.textBoxBingResults.Multiline = true;
+            this.textBoxBingResults.Name = "textBoxBingResults";
+            this.textBoxBingResults.Size = new System.Drawing.Size(941, 249);
+            this.textBoxBingResults.TabIndex = 2;
+            // 
+            // textBoxQuery
+            // 
+            this.textBoxQuery.Location = new System.Drawing.Point(98, 47);
+            this.textBoxQuery.Multiline = true;
+            this.textBoxQuery.Name = "textBoxQuery";
+            this.textBoxQuery.Size = new System.Drawing.Size(587, 140);
+            this.textBoxQuery.TabIndex = 3;
+            this.textBoxQuery.Text = "请输入要查询的内容";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(98, 496);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Bing搜索引擎";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(98, 204);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Sogou搜索引擎";
+            // 
+            // textBoxSogouResults
+            // 
+            this.textBoxSogouResults.Location = new System.Drawing.Point(98, 230);
+            this.textBoxSogouResults.Multiline = true;
+            this.textBoxSogouResults.Name = "textBoxSogouResults";
+            this.textBoxSogouResults.Size = new System.Drawing.Size(941, 263);
+            this.textBoxSogouResults.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1211, 816);
+            this.Controls.Add(this.textBoxSogouResults);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxQuery);
+            this.Controls.Add(this.textBoxBingResults);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox textBoxSogouResults;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.TextBox textBoxQuery;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxBingResults;
+
+        #endregion
+    }
+}
---
+++ 第十次作业/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        
+        private async void buttonSearch_Click(object sender, EventArgs e)
+        {
+            string query = textBoxQuery.Text;
+            textBoxSogouResults.Text = "Searching...";
+            textBoxBingResults.Text = "Searching...";
+
+            string sogouResults = await SearchEngineAsync("sogou", query);
+            string bingResults = await SearchEngineAsync("bing", query);
+
+            textBoxSogouResults.Text = sogouResults;
+            textBoxBingResults.Text = bingResults;
+        }
+        
+        private async Task<string> SearchEngineAsync(string engine, string query)
+        {
+            string url = (string.Equals(engine,"sogou")) ? $"https://www.sogou.com/web?query={query}" : $"https://www.bing.com/search?q={query}";
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
+            HttpResponseMessage response = await client.GetAsync(url);
+            string html = await response.Content.ReadAsStringAsync();
+
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            
+            string text = "";
+            var results = doc.DocumentNode.SelectNodes("//p[@class='txt-info']");
+            switch (engine)
+            {
+                case "sogou":
+                    results = doc.DocumentNode.SelectNodes("//p");
+                    if (results != null)
+                    {
+                        foreach (var result in results)
+                        {
+                            text += result.InnerText.Trim() + "\n";
+                            if (text.Length > 200) break;
+                        }
+                    }
+                    else
+                    {
+                        return "false";
+                    }
+                    break;
+                case "bing":
+                    results = doc.DocumentNode.SelectNodes("//p");
+                    if (results != null)
+                    {
+                        foreach (var result in results)
+                        {
+                            text += result.InnerText.Trim() + "\n";
+                            if (text.Length > 200) break;
+                        }
+                    }
+                    break;
+            }
+            
+            // if (engine == "sogou")
+            // {
+            //     var results = doc.DocumentNode.SelectNodes("//p[@class='txt-info']");
+            //     if (results != null)
+            //     {
+            //         foreach (var result in results)
+            //         {
+            //             text += result.InnerText.Trim() + "\n";
+            //             if (text.Length > 200) break;
+            //         }
+            //     }
+            //     else
+            //     {
+            //         return "false";
+            //     }
+            // }
+            // else if (engine == "bing")
+            // {
+            //     var results = doc.DocumentNode.SelectNodes("//p");
+            //     if (results != null)
+            //     {
+            //         foreach (var result in results)
+            //         {
+            //             text += result.InnerText.Trim() + "\n";
+            //             if (text.Length > 200) break;
+            //         }
+            //     }
+            // }
+
+            return text.Length > 200 ? text.Substring(0, 200) : text;
+        }
+
+
+        
+    }
+}
